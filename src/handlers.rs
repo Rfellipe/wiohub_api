26b233491e89531b650e069c9d790809f0f163d3
Fellipe Ribeiro <felliperibeiro1310@gmail.com>
2025-02-05 22:01:41 -0300

    optimized query

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,28 +1,14 @@
 use super::models::{Data, Device};
-use super::utils::{group_by_ten_minutes, process_data_with_fill, DeviceControllerQueries};
+use super::utils::DeviceControllerQueries;
 use super::{BsonRejection, MongoRejection};
 use bson::oid::ObjectId;
-use chrono::{DateTime, Duration, FixedOffset, Utc};
+use chrono::{DateTime, FixedOffset, Utc};
 use futures::TryStreamExt;
-use mongodb::bson::doc;
-use mongodb::options::FindOptions;
+use mongodb::{
+    bson::{doc, Document},
+};
 use mongodb::{Collection, Database};
-use serde::Serialize;
-use std::collections::HashMap;
 use std::convert::Infallible;
-
-#[derive(Debug, Serialize)]
-struct Foo {
-    field1: String,
-    field2: i32,
-    field3: bool,
-}
-
-#[derive(Debug, Serialize)]
-struct GroupedData {
-    device_id: ObjectId,
-    data: Data,
-}
 
 pub async fn device_controller(
     opts: DeviceControllerQueries,
@@ -49,6 +35,7 @@ pub async fn device_controller(
 
     let device_coll: Collection<Device> = db.collection("Device");
     let data_coll: Collection<Data> = db.collection("Data");
+
     // Fetch active device IDs
     let devices_id = device_coll
         .find(
@@ -67,31 +54,63 @@ pub async fn device_controller(
         .map(|doc| doc.id)
         .collect::<Vec<_>>();
 
-    // Fetch data for the devices within the time range
-    let all_data = data_coll
-        .find(
-            doc! {
+    // Fetch data for the devices within the time range and group by 10-minute intervals
+    let pipeline = vec![
+        doc! {
+            "$match": {
                 "deviceId": { "$in": devices_id },
                 "timestamp": {
                     "$gte": start_utc,
                     "$lte": end_utc,
                 },
             },
-            FindOptions::builder()
-                .sort(doc! { "timestamp": 1 })
-                .projection(doc! { "deviceId": 1, "timestamp": 1, "sensorType": 1, "value": 1 })
-                .build(),
-        )
+        },
+        doc! {
+            "$group": {
+                "_id": {
+                    "deviceId": "$deviceId",
+                    "interval": {
+                        "$dateToString": {
+                            "format": "%Y-%m-%dT%H:%M:00",
+                            "date": {
+                                "$dateTrunc": {
+                                    "date": "$timestamp",
+                                    "unit": "minute",
+                                    "binSize": 10,
+                                },
+                            },
+                        },
+                    },
+                    "sensorType": "$sensorType", 
+                },
+                "averageValue": { "$avg": "$value" }, 
+                "maxValue": {"$max": "$value"},
+                "minValue": {"$min": "$value"},
+                // "data": {
+                //     "$push": {
+                //         "timestamp": "$timestamp",
+                //         "sensorType": "$sensorType",
+                //         "value": "$value",
+                //     },
+                // },
+            },
+        },
+        doc! {
+            "$sort": {
+                "timestamp": 1,
+            },
+        },
+    ];
+
+    let all_data = data_coll
+        .aggregate(pipeline, None)
         .await
         .map_err(|e| warp::reject::custom(MongoRejection(e)))?
-        .try_collect::<Vec<_>>()
+        .try_collect::<Vec<Document>>()
         .await
         .map_err(|e| warp::reject::custom(MongoRejection(e)))?;
 
-    let grouped_data = group_by_ten_minutes(all_data);
-    let processed_data = process_data_with_fill(grouped_data, start_utc, end_utc);
-
-    Ok(warp::reply::json(&processed_data))
+    Ok(warp::reply::json(&all_data))
 }
 
 pub async fn hello_handler(
