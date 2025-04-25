c87c9dce83b490324c1d028c437190488171162f
Rfellipe <felliperibeiro1310@gmail.com>
2025-04-25 16:42:18 -0300

    http port and address changes

--- src/main.rs
+++ src/main.rs
@@ -269,7 +269,7 @@ async fn main() -> mongodb::error::Result<()> {
         .with(warp::wrap_fn(monitoring_wrapper));
 
     info!("starting http srv...");
-    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+    warp::serve(routes).run(([0, 0, 0, 0], 8162)).await;
 
     Ok(())
 }
